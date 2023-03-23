5a64ede0d80b74bf4c0e0854929b072ae2b08000
dandandanRamen <dandanmail90@gmail.com>
2023-03-23 00:01:21 +0000

    Added some operator overloads for Vec3
    Added some operator overloads, cleaned up some code.

--- Raytracing Project/main.cpp
+++ Raytracing Project/main.cpp
@@ -1,17 +1,12 @@
 
-/* Raytracing Project, by daniel simpson
-Adapted from Scratchapixel's Programming Blog.
-a Vector3 class (this is probably to simulate objects in 3d space)
-a Sphere class to draw these spheres.
-a trace function
-an overall render function that coalesces everything together */
+/* Raytracing Project, by Daniel Simpson
+Adapted from Scratchapixel's Programming Blog. */
 
 #include <iostream>
 
 ///
 /// Vector3 class to help place objects in 3D space.
 ///
-
 template <typename T>
 class Vec3
 {
@@ -21,25 +16,43 @@ private:
 	T m_z;
 
 public:
-	//constructors below.
-	Vec3(T x, T y, T z)
-		: m_x{ x }, m_y{ y }, m_z{ z } {}
+	///constructors
+	Vec3(T x, T y, T z) : m_x{ x }, m_y{ y }, m_z{ z } {}
+	Vec3() : m_x{ 0 }, m_y{ 0 }, m_z{ 0 } {}						
+	Vec3(T x) : m_x{x}, m_y{x}, m_z{x} {}
 
-	Vec3()
-		: m_x{0}, m_y{0}, m_z{0} {}
+	
+	///member functions
+	T getLengthSquared() { return (m_x * m_x) + (m_y * m_y) + (m_z * m_z); }  //magnitude is length
+	T getLength() { return m_x + m_y + m_z; }
+	Vec3& normalize()
+	{
+		T lengthSquared = getLengthSquared();
+	};
 
-	Vec3(T x)
-		: m_x{x}, m_y{x}, m_z{x} {}
-
-	//normalize function
-
-	//operator overloads below.
-
-	//
+	///operator overloads
+	friend Vec3 operator+(const Vec3& v, const Vec3& v2) { return v.m_x + v2.m_x, v.m_y + v2.m_y; }
+	friend Vec3 operator-(const Vec3& v, const Vec3& v2) { return v.m_x - v2.m_x, v.m_y - v2.m_y; }
+	friend std::ostream& operator<<(std::ostream& out, Vec3& v) { out << "(" << v.m_x << ", " << v.m_y << ") "; return out; }
 
 };
 
+typedef Vec3<float> Vec3f;
+
 int main()
 {
+	Vec3f point{ 2, 5, 2 };
 
+	std::cout << point.getLengthSquared() << '\n';
+	std::cout << point.getLength() << '\n';
+	
+	Vec3f point2{ point + point };
+	
+	std::cout << point2 << point << '\n';
 }
+
+
+//a Vector3 class (this is probably to simulate objects in 3d space)
+//a Sphere class to draw these spheres.
+//a trace function
+//an overall render function that coalesces everything together
