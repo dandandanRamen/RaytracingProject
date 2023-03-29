0d5a6c2432b3ce48bd96ddfc8276337dfe480c12
dandandanRamen <dandanmail90@gmail.com>
2023-03-29 11:55:23 +0100

    Moved Vec3 to seperate .cpp file
    Moved Vec3 to seperate .cpp file
    Added basic color test in main()

--- Raytracing Project/main.cpp
+++ Raytracing Project/main.cpp
@@ -1,72 +1,33 @@
-
-/* Raytracing Project, by Daniel Simpson
-Adapted from Scratchapixel's Programming Blog. */
+/* Raytracing Project, by Daniel Simpson 
+Adapted from Peter Shirley's "Raytracing in One Weekend" */
 
 #include <iostream>
 
-///
-/// Vector3 class to deal with specific points in 3D space. 
-///
-
-template <typename T>
-class Vec3
-{
-protected:
-	///member variables
-	T m_x;
-	T m_y;
-	T m_z;
-
-public:
-	///constructors
-	Vec3(T x, T y, T z) : m_x{ x }, m_y{ y }, m_z{ z } {}
-	Vec3() : m_x{ 0 }, m_y{ 0 }, m_z{ 0 } {}						
-	Vec3(T x) : m_x{x}, m_y{x}, m_z{x} {}
-
-
-	///member functions
-	T getLengthSquared() { return (m_x * m_x) + (m_y * m_y) + (m_z * m_z); } 
-
-	int getDot(const Vec3& v)
-	{
-		return (m_x * v.m_x) + (m_y * v.m_y) + (m_z * v.m_z);
-	};
-
-	Vec3& normalize()
-	{
-		T length = sqrt(getLengthSquared());	
-		if (length > 0.0) {
-			m_x /= length; m_y /= length; m_z /= length;
-		}
-		return *this;
-	};
-
-	///operator overloads
-	Vec3 operator + (const Vec3 &v) { return Vec3<T>{m_x + v.m_x, m_y + v.m_y, m_z + v.m_z}; }
-	Vec3 operator - (const Vec3 &v) { return Vec3<T>{m_x - v.m_x, m_y - v.m_y, m_z - v.m_z}; }
-	Vec3 operator * (const Vec3 &v) { return Vec3<T>{m_x * v.m_x, m_y * v.m_y, m_z * v.m_z}; }
-	Vec3 operator - () { return Vec3<T>{-m_x, -m_y, -m_z}; }
-	Vec3& operator += (const Vec3 &v) { m_x += v.m_x; m_y += v.m_y; m_z += v.m_z; return *this; }
-	Vec3& operator -= (const Vec3 &v) { m_x -= v.m_x; m_y -= v.m_y; m_z -= v.m_z; return *this; }
-	friend std::ostream& operator << (std::ostream& out, Vec3& v) 
-	{ 
-		out << "(" << v.m_x << ", " << v.m_y << ", " << v.m_z << ") "; 
-		return out; 
-	}
-};
-
-typedef Vec3<float> Vec3f;
-
 int main()
 {
-	Vec3f point{ 1.0, 2.0, 3.0};
-	Vec3f point2{ 2.0, 4.0, 6.0 };
-	std::cout << point.getDot(point2) << '\n';
-	
+	//Image Resolution
+
+	const int width = 256;
+	const int height = 256;
+
+	//Render
+
+	std::cout << "P3\n" << width << ' ' << height << "\n255\n";
+
+	for (int j = 0; j < height - 1; ++j) {
+		std::cerr << "\Scanlines remaining: " << j << ' ' << std::endl;	//writes progress to error output stream
+		for (int i = 0; i < width; ++i) {
+
+			double r = double(i) / (width - 1);
+			double g = double(j) / (width - 1);
+			double b = 0.25;
+
+			int i_r = static_cast<int>(255.999 * r);
+			int i_g = static_cast<int>(255.999 * g);
+			int i_b = static_cast<int>(255.999 * b);
+
+			std::cout << i_r << ' ' << i_g << ' ' << i_b << '\n';
+		}
+
+	}
 }
-
-
-//a Vector3 class (this is probably to simulate objects in 3d space)
-//a Sphere class to draw these spheres.
-//a trace function
-//an overall render function that coalesces everything together
