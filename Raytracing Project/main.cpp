062b70948377b72d5c7f8a0e37f9767e5cb4d7e7
dandandanRamen <dandanmail90@gmail.com>
2023-04-06 18:18:08 +0100

    surface normal colors added
    ray_color can now calculate surface normal shading instead of just one color

--- Raytracing Project/main.cpp
+++ Raytracing Project/main.cpp
@@ -8,26 +8,32 @@ Adapted from Peter Shirley's "Raytracing in One Weekend" */
 #include "vec3.h"
 
 //use the quadratic forumla to determine if the ray hits sphere. 
-bool hit_sphere(const vec3d& center, double radius, const ray& r)
+double hit_sphere(const vec3d& center, double radius, const ray& r)
 {
 	vec3d oc = r.getOrigin() - center;
 	double a = dotProduct(r.getDirection(), r.getDirection());
 	double b = 2.0 * dotProduct(oc, r.getDirection());
 	double c = dotProduct(oc, oc) - (radius * radius);
-
 	double discriminant = (b * b) - 4 * a * c;				//b^2 - 4ac
-	return (discriminant > 0);
+
+	if (discriminant < 0)
+		return -1.0;
+	else
+		return (-b - sqrt(discriminant)) / (2.0 * a);
 }
 
 //returns a vec3 color per ray (based on ray direction, unit vectors, t, y axis, etc)
 color3d ray_color(const ray& r)	
 {
-	if (hit_sphere(vec3d(0, 0, -1), 0.5, r))
-		return color3d(1, 0, 0);
+	double t = hit_sphere(vec3d(0, 0, -1), 0.5, r);
+	if (t > 0.0) {
+		vec3d normal = unitVector(r.at(t) - vec3d(0, 0, -1));
+		return 0.5 * color3d(normal.x() + 1, normal.y() + 1, normal.z() + 1);
+	}
 
 	//calculate ray color depending on direction of ray.
-	vec3d unit_direction = unitVector(r.getDirection());	//normalize direction vector
-	auto t = 0.5 * (unit_direction.y() + 1.0);			//times t * dir.y (y is height)
+	vec3d unit_direction = unitVector(r.getDirection());				//normalize direction vector
+	t = 0.5 * (unit_direction.y() + 1.0);								//t will be scaled with height y
 	vec3d blendedValue = (1.0 - t) * color3d(1.0, 1.0, 1.0) + t * color3d(0.5, 0.7, 1.0);
 	return blendedValue; 
 }
@@ -36,19 +42,19 @@ int main()
 {
 
 	//set image Resolution
-	const auto aspect_ratio = 16.0 / 9.0;
+	const double aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 
 	//set viewport (or camera) where we'll pass our rays through
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
+	double viewport_height = 2.0;
+	double viewport_width = aspect_ratio * viewport_height;
+	double focal_length = 1.0;
 
-	auto origin = vec3d(0, 0, 0);
-	auto horizontal = vec3d(viewport_width, 0, 0);
-	auto vertical = vec3d(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3d(0, 0, focal_length);
+	vec3d origin = vec3d(0, 0, 0);
+	vec3d horizontal = vec3d(viewport_width, 0, 0);
+	vec3d vertical = vec3d(0, viewport_height, 0);
+	vec3d lower_left_corner = origin - horizontal/2 - vertical/2 - vec3d(0, 0, focal_length);
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -56,8 +62,8 @@ int main()
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;  //shows progress
 		for (int i = 0; i < image_width; ++i) {
 
-			auto v = double(j) / (image_height - 1);	//scales height with itr
-			auto u = double(i) / (image_width - 1);		//scales width with itr
+			double v = double(j) / (image_height - 1);	//scales height with itr
+			double u = double(i) / (image_width - 1);		//scales width with itr
 
 			ray r(origin, (lower_left_corner + (u * horizontal) + (v * vertical) - origin)); //(origin, direction.)
 			color3d pixel_color = ray_color(r);
